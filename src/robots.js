d0fe88fa37f24fc8a920928d1601dfacbaeef71e
Tom Coleman <tom@cabranet.com>
2021-08-23 12:55:38 +0000

    Create robot cards

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Tom Coleman',
+      username: 'Coleman',
+      email: 'tom@coleman.co.uk'
+    },
+    {
+      id: 2,
+      name: 'Jane Evans',
+      username: 'Evans',
+      email: 'jane@surfspain.co.uk'
+    },
+    {
+      id: 3,
+      name: 'Jack the Mack',
+      username: 'JackMack',
+      email: 'jack@jack.es'
+    },
+    {
+      id: 4,
+      name: 'Richie Rich',
+      username: 'Richie',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
