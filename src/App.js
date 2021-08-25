cb21e58fbc9b7d4a86a53716c7f0b773bcd1e3f5
Tom Coleman <tom@cabranet.com>
2021-08-25 06:20:15 +0000

    Change fonts and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './scroll';
+import './app.css';
 
 class App extends Component {
     constructor() {
@@ -29,7 +31,9 @@ class App extends Component {
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots = {filteredRobots} />  
+                <Scroll>
+                    <CardList robots = {filteredRobots} />
+                </Scroll>
             </div> 
         )
     }
